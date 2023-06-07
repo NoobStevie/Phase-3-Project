65d48e0ca3de3b07d61d3861fb9ac8ded5065b96
Stevie <kamaustevek78@gmail.com>
2023-06-07 17:42:19 +0300

    Updated

---
+++ frontend/dunda/src/components/log_in.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Perform login logic here
+
+        // Reset form fields
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Log In</button>
+        </form>
+    );
+};
+
+export default LoginForm;
